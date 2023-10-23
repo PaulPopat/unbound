91fc668f1ebf5b717053421cefd6f12e500f7806
Paul Popat <paul@popatux.co.uk>
2023-10-23 08:45:24 +0000

    Tokeniser improvements

--- src/parser/tokeniser.ts
+++ src/parser/tokeniser.ts
@@ -2,64 +2,116 @@ import { Location } from "@location";
 import { ParserError } from "./error";
 import { Token } from "./token";
 
-const is_word_character = /^(?:[a-zA-Z0-9_]+|[=\-\/\\+?*<>]+)$/gm;
-const is_quote_mark = /^['"`]$/gm;
+class StringTraversal {
+  readonly #data: string;
+  #index: number = 0;
 
-export function* SplitTokens(code: string): Generator<Token> {
-  let start_line = 0;
-  let start_column = 0;
-  let line = 0;
-  let column = 0;
-  let current = "";
+  #line: number = 0;
+  #column: number = 0;
 
-  for (const char of code) {
-    if (current[0]?.match(is_quote_mark)) {
-      current += char;
-
-      if (char === "\n" && current[0] !== "`")
-        throw new ParserError(
-          new Location(start_line, start_column, line, column),
-          "Unexpected new line"
-        );
-
-      if (char === current[0]) {
-        yield new Token(
-          new Location(start_line, start_column, line, column + 1),
-          current.trim()
-        );
-        current = "";
-      }
-    } else {
-      if (!char.match(is_word_character) || !current.match(is_word_character)) {
-        if (current.trim())
-          yield new Token(
-            new Location(start_line, start_column, line, column),
-            current.trim()
-          );
-        current = "";
-      }
-
-      if (!current.trim()) {
-        start_line = line;
-        start_column = column;
-      }
-
-      current += char;
-    }
-
-    switch (char) {
-      case "\n":
-        line += 1;
-        column = 0;
-        break;
-      default:
-        column += 1;
-    }
+  constructor(data: string) {
+    this.#data = data;
   }
 
-  if (current.trim())
-    yield new Token(
-      new Location(start_line, start_column, line, column),
-      current.trim()
-    );
+  get current() {
+    return this.#data[this.#index] ?? "";
+  }
+
+  move() {
+    this.#index += 1;
+
+    switch (this.#data[this.#index]) {
+      case "\n":
+        this.#line += 1;
+        this.#column = -1;
+        break;
+      default:
+        this.#column += 1;
+    }
+
+    return this.#data[this.#index] ?? "";
+  }
+
+  get line() {
+    return this.#line;
+  }
+
+  get column() {
+    return this.#column;
+  }
+}
+
+const is_symbol_character = /^[=\-\/\\+?*<>]+$/gm;
+const is_word_character = /^[a-zA-Z0-9_]+$/gm;
+const is_quote_mark = /^['"`]$/gm;
+
+function VisitWord(data: StringTraversal): Token {
+  let result = data.current;
+  const start = { line: data.line, column: data.column };
+
+  while (data.move().match(is_word_character)) {
+    result += data.current;
+  }
+
+  return new Token(
+    new Location(start.line, start.column, data.line, data.column),
+    result
+  );
+}
+
+function VisitString(data: StringTraversal): Token {
+  let result = "";
+  const start = { line: data.line, column: data.column };
+
+  while (data.current !== result[0]) {
+    if (data.current === "\n" && result[0] !== "`")
+      throw new ParserError(
+        new Location(start.line, start.column, data.line, data.column),
+        "Unexpected new line"
+      );
+
+    result += data.current;
+    data.move();
+  }
+
+  result += data.current;
+  data.move();
+  return new Token(
+    new Location(start.line, start.column, data.line, data.column),
+    result
+  );
+}
+
+function VisitMaths(data: StringTraversal): Token {
+  let result = data.current;
+  const start = { line: data.line, column: data.column };
+
+  while (data.move().match(is_symbol_character)) {
+    result += data.current;
+  }
+
+  return new Token(
+    new Location(start.line, start.column, data.line, data.column),
+    result
+  );
+}
+
+export function* SplitTokens(code: string): Generator<Token> {
+  const data = new StringTraversal(code);
+
+  while (data.current) {
+    if (data.current.match(is_word_character)) yield VisitWord(data);
+    else if (data.current.match(is_symbol_character)) yield VisitMaths(data);
+    else if (data.current.match(is_quote_mark)) yield VisitString(data);
+    else if (data.current.trim()) {
+      yield new Token(
+        new Location(data.line, data.column, data.line, data.column + 1),
+        data.current.trim()
+      );
+
+      data.move();
+    } else {
+      data.move();
+    }
+  }
 }
